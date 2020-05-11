ee9e7b6b6f03bff2b27c3b93fabcbc14bb5bf773
xhercules <xhercules95@gmail.com>
2020-05-11 11:14:01 +0200

    path-modif

--- my_commit.sh
+++ my_commit.sh
@@ -4,5 +4,5 @@ BRANCH=${1?No branch specified}
 MESS=${2?No commit message specified}
 
 git add .
-git commit --author="xhercules <xhercules95.gmail.com>" -m $MESS
+git commit --author="xhercules <xhercules95@gmail.com>" -m $MESS
 git push origin $BRANCH
